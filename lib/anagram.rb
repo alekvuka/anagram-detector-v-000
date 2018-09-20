743c6164b15cff2d2bb080d714dbb449baf89b4d
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-20 20:33:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_matches)
+
+    arr_of_matches = Array.new
+
+    possible_matches.each do |w|
+      target_characters = @word.split("")
+      w = w.split("")
+      binding.pry
+    end
+  end
+end
